0837653706078ebdff2761ac2cf6b6ae753c11b0
Hemant-Nikam <hrn2501@gmail.com>
2026-07-07 09:49:25 +0530

    Time: 12 ms (15.38%), Space: 47 MB (26.5%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,79 +1,14 @@
 class Solution {
     public String convert(String s, int numRows) {
-        int ysize = 0;
-        int cnt = s.length();
-        boolean flag = false;
-        int tempx = 0;
-        while(cnt > 0){
-            if(!flag){
-                cnt = cnt - numRows;
-                ysize++;
-                tempx = numRows - 1;
-                flag = true;
-                
-            }
-            else{
-                if(tempx == 0){
-                    flag = false;
-                }
-                else{
-                    tempx--;
-                    ysize++;
-                    cnt = cnt - 1;
-                    System.out.println("ysize: " + ysize);
-                }
-            }
-            System.out.println("cnt: " + cnt);
-        }
-        System.out.println("ysize: " + ysize);
-        System.out.println("xsize: " + numRows);
-        // Map<charCharacter,Integer> map = new HashMap<>();
-        char arr[][] = new char[999 ][999];
-        int i = 0;
-        int x = 0 ;
-        int y = 0 ;
-        boolean up = false;
-        boolean side = false;
-        if(numRows == 1){
+        String ans= "";
+        if(numRows == 1)
             return s;
-        }
-        while(i < s.length()){
-            char c = s.charAt(i);
-            arr[x][y] = c;
-
-            if(!up){
-                x++;
-            }
-            else{
-                x--;
-            }
-            if(!side){
-                
-            }else{
-                y++;
-            }
-            if(x == numRows - 1){
-                up = true;
-                side = true;
-            }
-            if(x == 0){
-                up = false;
-                side = false;
-            }
-
-            i++;
-        }
-        return printArray(arr ,numRows, y);
-    }
-    public String printArray(char a[][] ,int x,int y){
-        String ans="";
-        for(int i = 0 ; i <= x ; i++){
-            for(int j = 0 ; j <= y ; j++){
-                if(a[i][j] != '\0'){
-                    ans = ans +""+ a[i][j] + "";
-                }
-                else{
-                    ans = ans;
+        for(int r=0;r< numRows;r++){
+            int inc = 2*(numRows-1);
+            for(int i = r;i<s.length();i = i+inc){
+                ans = ans + s.charAt(i);
+                if(r>0 && r < numRows -1 && i+inc - 2*r < s.length()){
+                    ans = ans +s.charAt(i+inc-2*r);
                 }
             }
         }
