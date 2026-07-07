ffed5f7ce541adfc09346899e78e3766d85560cc
Hemant-Nikam <hrn2501@gmail.com>
2026-07-07 10:04:27 +0530

    Time: 3 ms (96.28%), Space: 46.2 MB (94.67%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,17 +1,22 @@
 class Solution {
     public String convert(String s, int numRows) {
-        String ans= "";
+        // String ans= "";
+        StringBuilder ans = new StringBuilder();
+
         if(numRows == 1)
             return s;
         for(int r=0;r< numRows;r++){
             int inc = 2*(numRows-1);
             for(int i = r;i<s.length();i = i+inc){
-                ans = ans + s.charAt(i);
+                // ans = ans + s.charAt(i);
+                ans.append(s.charAt(i));
                 if(r>0 && r < numRows -1 && i+inc - 2*r < s.length()){
-                    ans = ans +s.charAt(i+inc-2*r);
+                    // ans = ans +s.charAt(i+inc-2*r);
+                    ans.append(s.charAt(i+inc-2*r));
                 }
             }
         }
-        return ans;
+        // return ans;
+        return ans.toString();
     }
 }
