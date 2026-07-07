316c7c60a9cc3799c7ccf3f0ffb55506ad21e588
Hemant-Nikam <hrn2501@gmail.com>
2026-07-07 11:44:46 +0530

    Time: 1 ms (100%), Space: 43.8 MB (59.65%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int myAtoi(String s) {
+        // System.out.println(Integer.MIN_VALUE);
+        // System.out.println(Math.pow(2,31) -1);
+        // System.out.println(Integer.MAX_VALUE);
+
+        long sign = 1;
+        long ans = 0;
+        int i = 0;
+        if(s.length()==0){
+            return 0;
+        }
+        
+        while(i < s.length() && s.charAt(i) == ' '){
+            i++;
+        }
+        
+        if(i < s.length() && s.charAt(i) == '-'){
+            i++;
+            sign = -1;
+        }
+        else if(i < s.length() && s.charAt(i) == '+'){
+            i++;
+        }
+
+        if(i < s.length() && Character.isDigit(s.charAt(i))){
+            while(i < s.length() && Character.isDigit(s.charAt(i)) ){
+                int digit = s.charAt(i) - '0';
+
+                if (ans > (Long.MAX_VALUE - digit) / 10) {
+                    return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+                }
+
+                ans = ans * 10 + digit;
+                i++;
+            }
+        }else{
+            return 0;
+        }
+        
+        if(sign == -1 && ans >= (long)Integer.MAX_VALUE +1){
+            return Integer.MIN_VALUE ;
+        }
+        else if(ans > Integer.MAX_VALUE ){
+            return Integer.MAX_VALUE ;
+        }
+        
+        return (int)(sign * ans);
+    }
+}
