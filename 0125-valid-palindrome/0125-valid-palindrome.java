6f07113fa15941daa8dfb379149a2cf464a780db
Hemant-Nikam <hrn2501@gmail.com>
2026-07-18 22:49:24 +0530

    Time: 147 ms (5.19%), Space: 47.6 MB (11.91%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -4,26 +4,21 @@ class Solution {
         int j = s.length() -1;
         if(s.length() == 0)
             return true;
-        while(i <= j && j > 0 && i < s.length()){
-            System.out.println("i , j : "+ i +" "+j );
-            if(Character.isLetterOrDigit(s.charAt(i)) && Character.isLetterOrDigit(s.charAt(j)) ){
-                if(Character.isLetter(s.charAt(i)) && Character.isLetter(s.charAt(j)) ){
-                    if(Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j)))
-                        return false;
-                } 
-                else if(s.charAt(i) != s.charAt(j))
+        while(i < j ){
+            char x = Character.isLetter(s.charAt(i))?Character.toLowerCase(s.charAt(i)) : s.charAt(i) ;
+            char y = Character.isLetter(s.charAt(j))?Character.toLowerCase(s.charAt(j)) : s.charAt(j) ;
+            
+            System.out.println("x : "+x+" y : "+y );
+            if(Character.isLetterOrDigit(x) && Character.isLetterOrDigit(y)){
+                if(x != y)
                     return false;
-                
                 i++;
                 j--;
             }
-            else if (!Character.isLetterOrDigit(s.charAt(i)))
+            else if(!Character.isLetterOrDigit(x))
                 i++;
-            else if(!Character.isLetterOrDigit(s.charAt(j))){
+            else if(!Character.isLetterOrDigit(y) )
                 j--;
-            }
-                
-            System.out.println("i , j :: "+ i +" "+j );
         }
         return true;
 
