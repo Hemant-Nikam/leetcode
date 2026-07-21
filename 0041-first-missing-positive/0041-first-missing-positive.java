2f3b26fc537b4b9fa397c1bae970981ae243f220
Hemant-Nikam <hrn2501@gmail.com>
2026-07-21 11:32:56 +0530

    Time: 1 ms (99.99%), Space: 71 MB (85.95%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,12 +1,11 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        //clean the array:
+        //cleaning the array:
         for(int i = 0 ; i< nums.length;i++){
             if(nums[i] < 1 || nums[i] > nums.length){
                 nums[i] = Integer.MAX_VALUE;
             }
         }
-        display(nums);
         //mark the numbers:
         for(int i = 0 ; i< nums.length;i++){
             int val = nums[i] > 0 ? nums[i] : nums[i] * -1;
@@ -14,21 +13,14 @@ class Solution {
                 nums[val - 1] =nums[val -1] > 0 ? nums[val -1] * (-1) : nums[val -1];
             }
         }
-        display(nums);
+        //checking the first positive number:
         for(int i = 0 ; i< nums.length;i++){
             if(nums[i] > 0 ){
                 return i+1;
             }
         }
-        display(nums);
+        //if not found anything:
         return nums.length+1;
 
-    }
-    public void display(int[] arr){
-
-        System.out.println();
-        for(int i = 0 ; i < arr.length;i++){
-            System.out.print(arr[i] + " ");
-        }
     }
 }
