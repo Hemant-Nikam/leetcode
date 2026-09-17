5d458101222432218fe10b542b730d06e338ee86
Hemant-Nikam <hrn2501@gmail.com>
2026-09-17 12:51:43 +0530

    Time: 0 ms (100%), Space: 44.2 MB (74.71%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -8,42 +8,55 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        // ListNode temp = list1;
-        if(list1 == null && list2 == null){
-            return list1;
-        }
-        else if(list1 == null){
+        if(list1 == null)
             return list2;
-        }
-        else if(list2 == null){
+        else if(list2 == null)
             return list1;
-        }
-        ListNode head = list1.val <= list2.val ? list1:list2;
-        ListNode temp = list1.val <= list2.val ? list2:list1;
-        list1 = head;
-        list2 = temp;
-        
-        while(list1 != null  && list2 != null){
-            if(list1.next != null && list1.val <= list2.val && list1.next.val >=list2.val ){
-                    ListNode t = list1;
-                    list1 = list2;
-                    list2 = t.next;
-                    t.next = list1;
-            }
-            else if(list1.next != null && list1.val <= list2.val && list1.next.val <list2.val){
-                    list1 = list1.next;
-            }
-            else if(list1.next == null ){
-                list1.next = list2;
-                list1 = null;
-
-            }
-
+        ListNode curr1 = new ListNode();
+        ListNode curr2 = new ListNode();
+        if(list1.val > list2.val)
+        {
+            curr1 = list2;
+            curr2 = list1;
 
         }
-        
+        else{
+            curr1 = list1;
+            curr2 = list2;
+        }
+        ListNode min = new ListNode();
+        ListNode temp = new ListNode();
+
+        ListNode head = curr1;
+        temp = curr1;
+        curr1 = curr1.next;
+
+        while(curr1 != null && curr2 != null)
+        {
+            if(curr1.val > curr2.val)
+            {
+                min = curr2;
+                curr2 = curr2.next;
+            }
+            else{
+                min = curr1;
+                curr1 = curr1.next;
+
+            }
+            temp.next = min;
+            temp = temp.next;
+            
+        }
+        if(curr1 == null){
+            temp.next = curr2;
+        }
+        else if(curr2 ==  null)
+        {
+            temp.next = curr1;
+        }
         return head;
     }
 }
